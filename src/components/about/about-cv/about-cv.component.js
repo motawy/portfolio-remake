7d0a61c627b626eb28abe6c5198972f3be177615
Mohammed Tantawy <mtantawy.mido@gmail.com>
2020-03-16 00:52:27 +0100

    Added resume

--- src/components/about/about-cv/about-cv.component.js
+++ src/components/about/about-cv/about-cv.component.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Resume from '../../../files/resume.pdf';
 import './about-cv.styles.scss';
 
 const AboutCV = () => {
   return (
     <div className="about-btn">
-      <a href="#">Download CV</a>
+      <a href={Resume} target="_blank" rel="noopener noreferrer">
+        Download CV
+      </a>
     </div>
   );
 };
