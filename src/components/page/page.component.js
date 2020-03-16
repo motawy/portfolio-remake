5ca7a081c9a32ba387b9bf58d3720d97750dd6ac
Mohammed Tantawy <mtantawy.mido@gmail.com>
2020-03-16 21:15:22 +0100

    Added Services

--- src/components/page/page.component.js
+++ src/components/page/page.component.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import Hero from '../hero/hero.component'
-import About from '../about/about.component'
-import './page.styles.scss'
-import Resume from '../resume/resume.component'
+import React from 'react';
+import Hero from '../hero/hero.component';
+import About from '../about/about.component';
+import './page.styles.scss';
+import Resume from '../resume/resume.component';
+import Services from '../services/services.component';
 
 const Page = () => {
-    return (
-        <div className="hero-wrapper">
-            <Hero />
-            <About />
-            <Resume />
-        </div>
-    )
-}
+  return (
+    <div className="hero-wrapper">
+      <Hero />
+      <About />
+      <Resume />
+      <Services />
+    </div>
+  );
+};
 
-export default Page
+export default Page;
