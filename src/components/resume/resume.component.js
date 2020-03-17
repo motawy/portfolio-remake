f5ad0aa25e964dcfc36d35d54ecadd092ca40a7a
Mohammed Tantawy <mtantawy.mido@gmail.com>
2020-03-17 23:49:42 +0100

    Reused title component

--- src/components/resume/resume.component.js
+++ src/components/resume/resume.component.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './resume.styles.scss';
-import Title from './title/title.component';
 import Education from './education/education.component';
 import Work from './work/work.component';
+import TitleSection from '../title-section/title-section.component';
 
 const Resume = () => {
   return (
     <section className="resume" id="resume">
       <div className="wrapper">
-        <Title />
+        <TitleSection firstPart="My" secondPart="Resume" description="Here's some of my most recent work." />
         <div className="main">
           <Education />
           <Work />
